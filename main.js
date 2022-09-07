4d0c48578a87b24eaf5b415d307c0201e0ae3863
julian vogel <birdman-coding@outlook.de>
2022-09-07 21:25:37 +0200

    Javascript 01

---
+++ main.js
@@ -0,0 +1,32 @@
+let myString = "Hallo String";
+
+let myString2 = 'Hallo String';
+
+let myString3 = `${myString2} ,ich bin Julian`
+
+const myNumber = 3;
+
+/*
+Ich bin ein mehrzeiliger Kommentar
+*/
+
+let boolean = true; // false
+
+let myUndefined = undefined;
+let myNull = null;
+
+let userName = null;
+
+
+//ab hier bin ich eingelogged
+userName = "Stan"; // db->users->id1
+
+
+//user hat sich wieder ausgelogged
+userName = null;
+
+let myDeclaration;
+console.log(myDeclaration);
+
+myDeclaration = "Peter";
+console.log(myDeclaration);
