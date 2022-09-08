780ef429d141b002a8d4ba6ee421defcc48fb306
julian vogel <birdman-coding@outlook.de>
2022-09-08 21:25:58 +0200

    lesson two

--- main.js
+++ main.js
@@ -1,32 +1,85 @@
-let myString = "Hallo String";
+const myvar1 = "string";
+let myvar2 = false;
+let myvar3 = 3;
+let myvar4 = undefined;
+let myvar5 = null;
 
-let myString2 = 'Hallo String';
+//arrays
+const number = 1;
+const myArr = [1, false, "Knäckebrot", null];
 
-let myString3 = `${myString2} ,ich bin Julian`
+console.log(myArr[2 - number]);
+myArr[2] = "Peter";
 
-const myNumber = 3;
+console.log(myArr[2]);
 
-/*
-Ich bin ein mehrzeiliger Kommentar
-*/
+//object
+const myObj = {
+  id: 1,
+  isInStore: false,
+  article: "Knäckebrot",
+  price: null,
+};
 
-let boolean = true; // false
+console.log(myObj.isInStore);
+console.log(myObj["price"]);
 
-let myUndefined = undefined;
-let myNull = null;
+console.dir(console);
 
-let userName = null;
+//nested Arrays
+
+const users = [
+  {
+    id: 1,
+    firstName: "Jenny",
+    email: "jenny@web.de",
+    password: "123456",
+    hobbies: ["coding", "sport", "eating"],
+  },
+  {
+    id: 2,
+    firstName: "Bernd",
+    email: "bernd@web.de",
+    password: "123456",
+    hobbies: ["coding", "sport", "eating"],
+  },
+  {
+    id: 3,
+    firstName: "Klaus",
+    email: "klaus@web.de",
+    password: "123456",
+    hobbies: ["coding", "sport", "eating"],
+  },
+];
+
+console.log(users[1].hobbies[1]);
+
+//name from DB
+const dbName = "Klaus";
+
+//Calculator
+function add(nummer1, nummer2) {    
+  console.log("hallo aus add");  
+  return nummer1 + nummer2;
+}
+
+function substract(nummer1, nummer2){
+    return nummer1 - nummer2;
+    console.log("hallo aus substract");  
+}
 
 
-//ab hier bin ich eingelogged
-userName = "Stan"; // db->users->id1
 
+function renderToScreen(result = 0) {
+  const div = document.querySelector("div");
+  const createdElement = document.createElement("h1");
+  createdElement.innerText = `Das Ergebnis ist ${result}`;
 
-//user hat sich wieder ausgelogged
-userName = null;
+  div.append(createdElement);
+}
 
-let myDeclaration;
-console.log(myDeclaration);
+const result1 = add(6, 4);
+const result2 = substract(10, 4)
 
-myDeclaration = "Peter";
-console.log(myDeclaration);
+renderToScreen(result1);
+renderToScreen(result2);
